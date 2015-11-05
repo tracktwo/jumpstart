fc32330450e75e360f4f9ebb5cd03f13772168af
tracktwo <jcemmett1@gmail.com>
2015-11-04 17:54:38 -0800

    Fix 1.5 release package to include JumpStart.u and modify the package script to error out if it can't find any of the files to copy.

--- PackageJumpStart.bat
+++ PackageJumpStart.bat
@@ -1,8 +1,28 @@
+@echo off
 mkdir %1
-copy %~dp0\Patches\JumpStart.txt %1
-copy %~dp0\INSTALL.txt %1
-copy %~dp0\README.txt %1
-copy %~dp0\CHANGELOG.txt %1
-copy D:\UDK_Sound\UDKGame\CookedPC\JumpStart.u %1
-copy %~dp0\DefaultJumpStart.ini %1
+copy %~dp0\Patches\JumpStart.txt %1 || (
+    echo Failed to copy JumpStart.txt 
+    exit /b 1
+    )
+copy %~dp0\INSTALL.txt %1 || (
+    echo Failed to copy INSTALL.txt 
+    exit /b 1
+    )
+copy %~dp0\README.txt %1 || (
+    echo Failed to copy README.txt
+    exit /b 1
+    )
+copy %~dp0\CHANGELOG.txt %1 || (
+    echo Failed to copy CHANGELOG.txt
+    exit /b 1
+    )
+copy D:\UDK\UDKGame\CookedPC\JumpStart.u %1 || (
+    echo Failed to copy JumpStart.u
+    exit /b 1
+    )
+copy %~dp0\DefaultJumpStart.ini %1 || (
+    echo Failed to copy DefaultJumpStart.ini
+    exit /b 1
+    )
+
 echo "I hope you remembered to cook the package!"
